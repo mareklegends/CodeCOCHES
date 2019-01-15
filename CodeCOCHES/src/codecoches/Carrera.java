231760d92982151b6e3c6b64697e06c04ac0537d
alumno <alumno@aula1-15>
2019-01-15 17:53:17 +0100

    lo principal 2

--- CodeCOCHES/src/codecoches/Carrera.java
+++ CodeCOCHES/src/codecoches/Carrera.java
@@ -24,35 +24,79 @@ public class Carrera {
     }
     
 
+    public void añadirPiloto(Coche nobot){
+        for (int i = 0; i < 1; i++) {
+            if (pilotos[i]==null) {
+                pilotos[i]=nobot;
+                System.out.println(">>>> Tu coche esta añadido ");
+                break;
+            }
+        }
+         
+    }
    
      
     
-    public void añadirBots(){
+    public void añadirBots(){        
+        boolean encontrado = false;
+        boolean rellenado = false;
+        int nramdomfimal=0;
+  
+        //Sacar dorsal
         
-        
+        do{
+        Random r = new Random();
+        int nramdom = r.nextInt(4)+1;
         for (int i = 1; i < pilotos.length; i++) {
-            
-            Random r = new Random();
-            int nramdom = r.nextInt(4)+1;
-           
-            
-            if (pilotos[i].getDorsal()!=nramdom) { 
-                
-                pilotos[i].setDorsal(nramdom);
+            if (pilotos[i]!=null && pilotos[i].getDorsal()!=nramdom) { 
+                encontrado = true;
+                nramdomfimal=nramdom;
+                break;
             } 
+        }   
+        }while(encontrado!=true);
+        
+        if (encontrado==true) {
             
-            String[] nomramdom = {"mango", "uva", "pera", "manzana", "melon", "sandia"};
-            String anterior=nomramdom[nramdom];
+            do{
+            Random r = new Random();
+            int nramdom2 = r.nextInt(6)+1;
+            String[] nomramdom = {"mango", "uva", "pera", "manzana", "melon", "sandia", "platano", "piña"};
+            String nbot=nomramdom[nramdom2];
             
-            if (pilotos[i].getNombrepiloto()!=anterior) { 
-           
-             pilotos[i].setNombrepiloto(nomramdom[nramdom]);
-             
+            for (int i = 1; i < pilotos.length; i++) {
+                
+                   if (pilotos[i]==null) { 
+                
+                        Coche bot = new Coche(nbot, nramdomfimal, 10);
+                        pilotos[i]=bot;
+                        break;
+                
+                   }
+                   
             }
-        }  
-        
-        
+            
+           }while(rellenado!=true);
+            
+        } 
     }
     
     
-}
+        
+      
+  public void mostrarPilotos(){
+      for (int i = 0; i < pilotos.length; i++) {
+          if (pilotos[i]!=null) {
+                   
+          System.out.println(i+""+pilotos[i]);
+             }
+      }
+  }         
+      
+        
+    
+            
+    }
+    
+    
+
--- CodeCOCHES/src/codecoches/CodeCOCHES.java
+++ CodeCOCHES/src/codecoches/CodeCOCHES.java
@@ -11,6 +11,15 @@ public class CodeCOCHES {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+         Carrera a = new Carrera("CarreraCAR", 10);
+        
+        Coche nobot = new Coche("Marek", 26, 10);
+        
+       a.añadirPiloto(nobot);
+        
+        a.añadirBots();
+        
+        a.mostrarPilotos();
         
         
         
